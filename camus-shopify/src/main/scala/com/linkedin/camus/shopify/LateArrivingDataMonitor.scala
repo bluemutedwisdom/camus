c3759f3655b232a00e1748325e031bfb2e2ea3b3
Olessia Karpova <olessia.karpova@shopify.com>
2017-11-16 16:47:31 -0500

    Add a new way to check for LAD: based on paths touched in the last N runs.

--- camus-shopify/src/main/scala/com/linkedin/camus/shopify/LateArrivingDataMonitor.scala
+++ camus-shopify/src/main/scala/com/linkedin/camus/shopify/LateArrivingDataMonitor.scala
@@ -7,14 +7,16 @@ import com.linkedin.camus.etl.kafka.reporter.StatsdReporter
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.log4j.Logger
-import org.wikimedia.analytics.refinery.job.CamusStatusReader
 import scopt.OptionParser
 
 object LateArrivingDataMonitor {
   val log: Logger = Logger.getLogger(LateArrivingDataMonitor.getClass)
 
   case class Params(camusPropertiesFilePath: String = "",
-                    window: Long = 1000 * 60 * 60 * 6) // 6 hours window
+                    checkType: String = "window",
+                    window: Long = 1000 * 60 * 60 * 6, // 6 hours window
+                    numRuns: Int = 1
+                   )
 
   val argsParser = new OptionParser[Params]("com.linkedin.camus.shopify.LateArrivingDataMonitor") {
     head("Camus Late Arriving Data Checker", "")
@@ -26,17 +28,44 @@ object LateArrivingDataMonitor {
       p.copy(camusPropertiesFilePath = x)
     } text "Camus configuration properties file path."
 
+    opt[String]('t', "check-type") required() action { (x, p) =>
+      p.copy(checkType = x)
+    } text "Camus check type: \"runs\" or \"window\""
+
     opt[Long]('w', "window") valueName "<window>" action { (x, p) =>
       p.copy(window = x)
-    } text "Camus configuration properties file path."
+    } text "Hours before last offset to check: only applicable in \"window\" type check"
+
+    opt[Int]('r', "num-runs") optional() valueName "<int>" action { (x, p) =>
+      p.copy(numRuns = x)
+    } text "Number of camus runs to check: only applicable in \"runs\" type check."
+
   }
 
 
   def checkDropsInWindow(properties: Properties, fs: FileSystem, window: Long): Boolean = {
+    log.info(s"Running check on folders dropped in the window of $window milliseconds.")
     log.info("Fetching topics and folders to check...")
     val camusExecutions = new CamusExecutions(properties, fs)
-    log.info(s"Checking folders in window ($window) to flag violation...")
     val foldersToCheck = camusExecutions.droppedFoldersInWindow(window)
+
+    log.info(s"Checking folders to flag violation...")
+    checkDrops(properties, fs, foldersToCheck.toList)
+  }
+
+  def checkDropsInRuns(properties: Properties, fs: FileSystem, runs: Int): Boolean = {
+    log.info(s"Running check on folders dropped in the last $runs Camus runs.")
+    val camusExecutions = new CamusExecutions(properties, fs)
+    val camusRunPaths = camusExecutions.lastRunDirs(runs)
+
+    log.info(s"Collecting directories touched in $runs last Camus runs: $camusRunPaths")
+    val foldersToCheck = camusExecutions.droppedFoldersInRuns(camusRunPaths)
+
+    log.info(s"Checking folders to flag violation...")
+    checkDrops(properties, fs, foldersToCheck.toList)
+  }
+
+  def checkDrops(properties: Properties, fs: FileSystem, foldersToCheck: List[String]): Boolean = {
     var violationFound = false
     foldersToCheck.foreach(
       folder => {
@@ -63,14 +92,21 @@ object LateArrivingDataMonitor {
   def main(args: Array[String]): Unit = {
     argsParser.parse(args, Params()) match {
       case Some (params) =>
+        if (! (params.checkType == "window" ||  params.checkType == "runs")) {
+          log.error(s"ERROR: Unknown check type ${params.checkType}")
+          sys.exit(1)
+        }
         log.info("Setting up...")
         val fs = FileSystem.get(new Configuration())
-        val camusReader = new CamusStatusReader(fs)
         val properties: Properties = new Properties()
         properties.load(new FileInputStream(params.camusPropertiesFilePath))
 
-        val violationFound = checkDropsInWindow(properties, fs, params.window)
-
+        val violationFound =
+          if (params.checkType == "window") {
+           checkDropsInWindow(properties, fs, params.window)
+        } else { // runs
+          checkDropsInRuns(properties, fs, params.numRuns)
+        }
         if (violationFound) {
           log.error("ERROR: One or more folders have late-arriving data.")
           sys.exit(1)
